abddd7f3a05b162394b3ce2e4b972e8e655c93f2
Marcin Bartmiński <mbdjj@o2.pl>
2023-03-16 09:19:15 +0100

    Initial Commit

---
+++ whenMatura/whenMaturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  whenMaturaApp.swift
+//  whenMatura
+//
+//  Created by Marcin Bartminski on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct whenMaturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
