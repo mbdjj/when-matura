95c73293118a4a6cbd5320807b47c2fc55717e07
Marcin Bartmiński <mbdjj@o2.pl>
2023-03-24 07:47:03 +0100

    :sparkles: Add more themes and theme preview

--- whenMatura/Managers/ThemeManager.swift
+++ whenMatura/Managers/ThemeManager.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 class ThemeManager: ObservableObject {
     
-    @Published var current = Theme.blueAlt
+    @Published var current = Theme.indigo
     
-    let themes: [Theme] = [.defaultTheme, .blue, .blueAlt]
+    let defaultThemes: [Theme] = [.defaultTheme]
+    let colorThemes: [Theme] = [.blue, .red, .yellow, .orange, .indigo]
+    let altThemes: [Theme] = [.blueAlt, .redAlt, .yellowAlt, .orangeAlt, .indigoAlt]
     
     static let shared = ThemeManager()
     
@@ -23,13 +25,20 @@ struct Theme: Hashable {
     let background: Color
     
     var preview: some View {
-        HStack {
-            if background == .init(uiColor: .systemBackground) {
+        HStack(spacing: 0) {
+            if background == .systemBackground {
                 background
                 primary
             } else {
                 background
             }
+        }
+        .frame(width: 40, height: 40)
+        .cornerRadius(20)
+        .padding(2)
+        .background {
+            Color.primary
+                .clipShape(Circle())
         }
     }
     
@@ -41,13 +50,23 @@ struct Theme: Hashable {
     
     init(primary: Color, background: Color) {
         self.primary = primary
-        self.secondary = primary.opacity(0.9)
+        self.secondary = primary.opacity(0.6)
         self.background = background
     }
     
     static let defaultTheme = Theme(primary: .primary, secondary: .gray, background: .systemBackground)
+    
     static let blue = Theme(primary: .white, background: .blue)
+    static let red = Theme(primary: .white, background: .red)
+    static let yellow = Theme(primary: .black, background: .yellow)
+    static let orange = Theme(primary: .black, background: .orange)
+    static let indigo = Theme(primary: .white, background: .indigo)
+    
     static let blueAlt = Theme(primary: .blue, background: .systemBackground)
+    static let redAlt = Theme(primary: .red, background: .systemBackground)
+    static let yellowAlt = Theme(primary: .yellow, background: .systemBackground)
+    static let orangeAlt = Theme(primary: .orange, background: .systemBackground)
+    static let indigoAlt = Theme(primary: .indigo, background: .systemBackground)
 }
 
 extension Color {
--- whenMatura/Views/ChooseThemeView.swift
+++ whenMatura/Views/ChooseThemeView.swift
@@ -8,17 +8,84 @@
 import SwiftUI
 
 struct ChooseThemeView: View {
+    
+    @ObservedObject var manager = ThemeManager.shared
+    
     var body: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16, alignment: .center), count: 3)) {
-            ForEach(ThemeManager.shared.themes, id: \.self) { theme in
-                Button {
-                    ThemeManager.shared.current = theme
-                } label: {
-                    theme.preview
-                        .frame(width: 40, height: 40)
-                        .cornerRadius(20)
+        VStack {
+            
+            let selected = manager.current
+            VStack {
+                Text("Podgląd motywu")
+                    .font(.system(.title, design: .rounded, weight: .bold))
+                    .foregroundColor(selected.primary)
+                Text("\(Int.random(in: 1 ... 1000))")
+                    .font(.system(size: 100, weight: .semibold, design: .rounded))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.6)
+                    .foregroundColor(selected.primary)
+                Text("To liczba, która się wylosowała")
+                    .font(.system(.title3, design: .rounded))
+                    .bold()
+                    .foregroundColor(selected.secondary)
+            }
+            .padding(24)
+            .background {
+                selected.background
+                    .cornerRadius(16)
+            }
+            .padding(2)
+            .background {
+                Color.primary
+                    .cornerRadius(18)
+            }
+            .padding(8)
+            .minimumScaleFactor(0.6)
+            
+            Spacer()
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16, alignment: .center), count: 4)) {
+                ForEach(ThemeManager.shared.defaultThemes, id: \.self) { theme in
+                    Button {
+                        withAnimation {
+                            ThemeManager.shared.current = theme
+                        }
+                    } label: {
+                        theme.preview
+                    }
                 }
             }
+            
+            Spacer()
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16, alignment: .center), count: 4)) {
+                ForEach(ThemeManager.shared.colorThemes, id: \.self) { theme in
+                    Button {
+                        withAnimation {
+                            ThemeManager.shared.current = theme
+                        }
+                    } label: {
+                        theme.preview
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16, alignment: .center), count: 4)) {
+                ForEach(ThemeManager.shared.altThemes, id: \.self) { theme in
+                    Button {
+                        withAnimation {
+                            ThemeManager.shared.current = theme
+                        }
+                    } label: {
+                        theme.preview
+                    }
+                }
+            }
+            
+            Spacer()
+            
         }
     }
 }
